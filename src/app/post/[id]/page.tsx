18bd9ce1486fc94670558b0c8797b4875b9af844
Keroles Siha <kerolessiha999@gmail.com>
2025-06-21 18:53:27 +0300

    fix: rename PageProps to avoid Vercel type conflict

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -6,20 +6,20 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Posts from "@/app/posts/page";
 
-type PageProps = {
+type PostPageProps = {
   params: {
     id: string;
   };
 };
 
-export default function PostPage({ params }: PageProps) {
+export default function PostPage({ params }: PostPageProps) {
   const dispatch = useDispatch<dispatchType>();
 
   const { singlePost } = useSelector((state: stateType) => state.post);
 
   useEffect(() => {
     dispatch(getSinglePost(params.id));
-  }, [dispatch, params.id]); // ✅ إضافة dependencies هنا كمان لتفادي تحذير React
+  }, [dispatch, params.id]);
 
   return (
     <>
